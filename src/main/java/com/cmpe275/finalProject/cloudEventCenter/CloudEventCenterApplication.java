17ac7d8f91cfbaa7bc254ada828bf51e64a4279c
Adarsh Murthy <adarsh.narasimhamurthy@sjsu.edu>
2022-04-20 21:33:59 -0700

    Spring boot setup

---
+++ src/main/java/com/cmpe275/finalProject/cloudEventCenter/CloudEventCenterApplication.java
@@ -0,0 +1,13 @@
+package com.cmpe275.finalProject.cloudEventCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEventCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEventCenterApplication.class, args);
+	}
+
+}
