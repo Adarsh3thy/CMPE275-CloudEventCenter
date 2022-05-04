bf7f32745c01f54c739d19a1f629466f17fec427
Anay Dilip Naik <89814957+anaydilipnaik@users.noreply.github.com>
2022-05-03 22:03:39 -0700

    login and register - design completed (functionality pending)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ frontend/src/components/Register/index.js
@@ -0,0 +1,285 @@
+import React, { useState, useEffect } from "react";
+import Image52 from "../../assets/google-logo.png";
+import UndrawImage from "../../assets/isometric.png";
+import arrowUp from "../../assets/arrow-up.svg";
+import { logo } from "../../utils/constants";
+import { TextField, Typography, Button, Grid } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import { useLocation } from "react-router-dom";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const Register = () => {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  // TODO: handlers
+
+  return (
+    <Grid
+      container
+      direction="row"
+      style={{
+        width: "100%",
+        height: "100%",
+        background: "#ffffff",
+      }}
+    >
+      <Grid item md={1.5} lg={1.5} xl={1.5} />
+      <Grid
+        item
+        container
+        direction="column"
+        xs={12}
+        sm={12}
+        md={4}
+        lg={4}
+        xl={4}
+      >
+        <Grid item>
+          <img
+            src={logo}
+            style={{
+              width: "239.58px",
+              height: "112.93px",
+              cursor: "pointer",
+              marginTop: "84.27px",
+            }}
+          />
+        </Grid>
+        <Grid item>
+          <span
+            style={{
+              fontFamily: "Work Sans",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              fontSize: "18px",
+              color: "#000000",
+            }}
+          >
+            Organizing events has never been that easy.
+          </span>
+        </Grid>
+        <Grid item>
+          <Typography
+            style={{
+              width: "116px",
+              height: "45px",
+              fontFamily: "Work Sans",
+              fontStyle: "normal",
+              fontWeight: "bold",
+              fontSize: "30px",
+              color: "#000000",
+              marginTop: "25px",
+            }}
+          >
+            Sign Up
+          </Typography>
+        </Grid>
+        <form
+        // onSubmit={handleSubmit}
+        >
+          <Grid item>
+            <TextField
+              required
+              type="email"
+              placeholder="Email Address"
+              value={email}
+              style={{
+                width: "395px",
+                height: "67px",
+                boxSizing: "border-box",
+                borderRadius: "5px",
+                marginTop: "30px",
+              }}
+              error={error != "" ? true : false}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              required
+              type="password"
+              placeholder="Password"
+              style={{
+                width: "395px",
+                height: "67px",
+                boxSizing: "border-box",
+                borderRadius: "5px",
+                marginTop: "5px",
+              }}
+              onChange={(e) => setPassword(e.target.value)}
+              error={error != "" ? true : false}
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              type="submit"
+              style={{
+                width: "395px",
+                height: "68px",
+                cursor: "pointer",
+                background: "#8A2BE2",
+                borderRadius: "5px",
+                color: "#ffffff",
+                textTransform: "none",
+                fontFamily: "Work Sans",
+                fontStyle: "normal",
+                fontWeight: "bold",
+                fontSize: "18px",
+                marginTop: "12px",
+              }}
+            >
+              <img
+                src={arrowUp}
+                style={{
+                  position: "static",
+                  width: "24px",
+                  height: "24px",
+                  flex: "none",
+                  order: 0,
+                  flexGrow: 0,
+                  margin: "0px 8px",
+                }}
+              />
+              Register
+            </Button>
+          </Grid>
+        </form>
+        <Grid item>
+          <div
+            style={{
+              height: "28px",
+              fontFamily: "Work Sans",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              fontSize: "18px",
+              lineHeight: "27px",
+              color: "#000000",
+              opacity: 0.5,
+              marginTop: "25px",
+            }}
+          >
+            Or Sign Up with:
+          </div>
+        </Grid>
+        <Grid item container direction="row">
+          <Grid
+            item
+            style={{
+              marginTop: "15px",
+              marginRight: "15px",
+            }}
+          >
+            <div
+              style={{
+                width: "100px",
+                height: "68px",
+                cursor: "pointer",
+                background: "#E6E6FA",
+                border: "1px solid rgba(33, 131, 223, 0.52)",
+                boxSizing: "border-box",
+                borderRadius: "5px",
+                textAlign: "center",
+              }}
+              // onClick={handleSjsuLogin}
+            >
+              <img
+                src={Image52}
+                style={{
+                  width: "48px",
+                  height: "48px",
+                  cursor: "pointer",
+                  marginTop: "10px",
+                }}
+              />
+            </div>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          container
+          direction="row"
+          style={{
+            marginTop: "25px",
+            fontFamily: "Work Sans",
+            fontStyle: "normal",
+            fontWeight: "normal",
+            fontSize: "18px",
+          }}
+        >
+          <Grid
+            item
+            style={{
+              color: "#000000",
+              opacity: 0.5,
+            }}
+          >
+            <div>Already have an account?</div>
+          </Grid>
+          <Grid item>
+            <a
+              style={{
+                cursor: "pointer",
+                color: "#8A2BE2",
+                fontWeight: "bold",
+                marginLeft: "10px",
+              }}
+              onClick={() => {
+                window.location.href = "/login";
+              }}
+            >
+              Sign In Now
+            </a>
+          </Grid>
+        </Grid>
+        <Stack sx={{ width: "100%" }}>
+          <Snackbar
+            open={open}
+            autoHideDuration={6000}
+            // onClose={handleClose}
+          >
+            <Alert
+              // onClose={handleClose}
+              severity={success ? "success" : "error"}
+              sx={{ width: "100%" }}
+            >
+              {success ? success : error}
+            </Alert>
+          </Snackbar>
+        </Stack>
+      </Grid>
+      <Grid
+        item
+        sx={{
+          display: {
+            xs: "none",
+            sm: "none",
+            md: "block",
+            lg: "block",
+            xl: "block",
+          },
+        }}
+        md={6.5}
+        lg={6.5}
+        xl={6.5}
+      >
+        <img
+          src={UndrawImage}
+          style={{
+            marginTop: "30px",
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Register;
