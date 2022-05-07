e0dd87433849944c1c65e9289615eb0ea3cbf0a5
Anay Dilip Naik <89814957+anaydilipnaik@users.noreply.github.com>
2022-05-06 22:01:13 -0700

    account popover set

--- frontend/src/components/Dashboard/AccountPopover.js
+++ frontend/src/components/Dashboard/AccountPopover.js
@@ -17,17 +17,12 @@ const MENU_OPTIONS = [
   {
     label: "Home",
     icon: "eva:home-fill",
-    linkTo: "/",
+    linkTo: "/update-user",
   },
   {
     label: "Profile",
     icon: "eva:person-fill",
-    linkTo: "#",
-  },
-  {
-    label: "Settings",
-    icon: "eva:settings-2-fill",
-    linkTo: "#",
+    linkTo: "/update-user",
   },
 ];
 
