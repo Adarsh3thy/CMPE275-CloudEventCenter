377863d0d616f7c34df8e627e32d3561a1d22ee5
Anay Dilip Naik <89814957+anaydilipnaik@users.noreply.github.com>
2022-05-06 21:43:44 -0700

    unused components deleted

--- frontend/src/components/Dashboard/DashboardNavbar.js
+++ frontend/src/components/Dashboard/DashboardNavbar.js
@@ -2,10 +2,7 @@ import PropTypes from "prop-types";
 import { alpha, styled } from "@mui/material/styles";
 import { Box, Stack, AppBar, Toolbar, IconButton } from "@mui/material";
 import Iconify from "../../mui_components/Iconify";
-import Searchbar from "./Searchbar";
 import AccountPopover from "./AccountPopover";
-import LanguagePopover from "./LanguagePopover";
-import NotificationsPopover from "./NotificationsPopover";
 
 const DRAWER_WIDTH = 280;
 const APPBAR_MOBILE = 64;
@@ -44,7 +41,6 @@ export default function DashboardNavbar({ onOpenSidebar }) {
           <Iconify icon="eva:menu-2-fill" />
         </IconButton>
 
-        <Searchbar />
         <Box sx={{ flexGrow: 1 }} />
 
         <Stack
