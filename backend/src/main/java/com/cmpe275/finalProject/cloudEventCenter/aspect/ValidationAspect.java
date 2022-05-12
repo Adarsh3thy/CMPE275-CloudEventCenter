a816a1a589455f952fdd0c048b44b17d1bc1ca59
Shreyas Kulkarni <Shreyasrameshkulkarni@gmail.com>
2022-05-11 21:35:27 -0700

    Added validation for create Event

--- backend/src/main/java/com/cmpe275/finalProject/cloudEventCenter/aspect/ValidationAspect.java
+++ backend/src/main/java/com/cmpe275/finalProject/cloudEventCenter/aspect/ValidationAspect.java
@@ -23,46 +23,46 @@ public class ValidationAspect {
 	@Autowired
 	private UserRepository userRepository;
 	
-//	@Before("execution(public * com.cmpe275.finalProject.cloudEventCenter.service.EventService.addEvent(..)) && args(eventData)")
-//	public void CreateEventValidationAdvice(JoinPoint joinPoint, EventData eventData) {
-//		
-//		System.out.printf("Permission check before the executuion of the method %s\n", joinPoint.getSignature().getName());
-//		
-//		//Validation should be updated
-//		if(eventData.getTitle().isBlank() || eventData.getDescription().isBlank())
-//			throw new IllegalArgumentException("Enter a valid title and desciption");
-//		
-//		if(eventData.getStartTime() == null || eventData.getEndTime() == null)
-//			throw new IllegalArgumentException("Enter a valid startTime/endTime");
-//		
-//		if(eventData.getEndTime().isBefore(eventData.getStartTime()) || eventData.getEndTime().isEqual(eventData.getStartTime())) {
-//			throw new IllegalArgumentException("Enter a startTime before endTime");
-//		}
-//			
-//		if(eventData.getStartTime().isBefore(eventData.getDeadline())) {
-//			throw new IllegalArgumentException("Enter a startTime after deadline");
-//		}
-//		
-//		if(eventData.getCity().isBlank() || eventData.getState().isBlank() || eventData.getZip().isBlank()) {
-//			throw new IllegalArgumentException("Enter a city, state and zipcode for the event");
-//		}
-//		
-//		if(eventData.getMinParticipants() < 0) {
-//			throw new IllegalArgumentException("Enter a positive minimum number of participants");
-//		}
-//		
-//		if(eventData.getMaxParticipants() > Integer.MAX_VALUE) {
-//			throw new IllegalArgumentException("Enter a valid number of max participants");
-//		}
-//		
-//		User organizer  = userRepository.findById(eventData.getOrganizerID()).orElse(null);
-//		Role organizerRole = organizer.getRoles().iterator().next();
-//		
-//		if(eventData.getFee() > 0 && organizerRole.getName().equals(ERole.ROLE_PERSON)) {
-//			throw new IllegalArgumentException("Participant cant charge a fee");
-//		}
-//		
-//	}
+	@Before("execution(public * com.cmpe275.finalProject.cloudEventCenter.service.EventService.addEvent(..)) && args(eventData)")
+	public void CreateEventValidationAdvice(JoinPoint joinPoint, EventData eventData) {
+		
+		System.out.printf("Permission check before the executuion of the method %s\n", joinPoint.getSignature().getName());
+		
+		//Validation should be updated
+		if(eventData.getTitle().isBlank() || eventData.getDescription().isBlank())
+			throw new IllegalArgumentException("Enter a valid title and desciption");
+		
+		if(eventData.getStartTime() == null || eventData.getEndTime() == null)
+			throw new IllegalArgumentException("Enter a valid startTime/endTime");
+		
+		if(eventData.getEndTime().isBefore(eventData.getStartTime()) || eventData.getEndTime().isEqual(eventData.getStartTime())) {
+			throw new IllegalArgumentException("Enter a startTime before endTime");
+		}
+			
+		if(eventData.getStartTime().isBefore(eventData.getDeadline())) {
+			throw new IllegalArgumentException("Enter a startTime after deadline");
+		}
+		
+		if(eventData.getAddress().getCity().isBlank() || eventData.getAddress().getState().isBlank() || eventData.getAddress().getZip().isBlank()) {
+			throw new IllegalArgumentException("Enter a city, state and zipcode for the event");
+		}
+		
+		if(eventData.getMinParticipants() < 0) {
+			throw new IllegalArgumentException("Enter a positive minimum number of participants");
+		}
+		
+		if(eventData.getMaxParticipants() > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException("Enter a valid number of max participants");
+		}
+		
+		User organizer  = userRepository.findById(eventData.getOrganizerID()).orElse(null);
+		Role organizerRole = organizer.getRoles().iterator().next();
+		
+		if(eventData.getFee() > 0 && organizerRole.getName().equals(ERole.ROLE_PERSON)) {
+			throw new IllegalArgumentException("Participant cant charge a fee");
+		}
+		
+	}
 	
 	@Before("execution(public * com.cmpe275.finalProject.cloudEventCenter.service.EventService.getEventByID(..)) && args(id)")
 	public void getEventValidationAdvice(JoinPoint joinPoint, String id) {
