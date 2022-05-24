7db3429dc938a2594fb47b58aa793574f56751ec
Adarsh Murthy <adarsh.narasimhamurthy@sjsu.edu>
2022-05-24 15:24:55 -0700

    Including city in signin

--- backend/src/main/java/com/cmpe275/finalProject/cloudEventCenter/POJOs/JwtResponse.java
+++ backend/src/main/java/com/cmpe275/finalProject/cloudEventCenter/POJOs/JwtResponse.java
@@ -12,13 +12,15 @@ public class JwtResponse {
 	  private String refreshToken;
 	  private String id;
 	  private String email;
+	  private String city;
 	  private List<String> roles;
 	
-	public JwtResponse(String accessToken,String refreshToken, String id, String username, String email, List<String> roles) {
+	public JwtResponse(String accessToken,String refreshToken, String id, String username, String email,String city, List<String> roles) {
 	    this.token = accessToken;
 	    this.refreshToken = refreshToken;
 	    this.id = id;
 	    this.email = email;
+	    this.city=city;
 	    this.roles = roles;
 	  }
 	
