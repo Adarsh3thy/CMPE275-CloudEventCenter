3fbac50abd8b0f14c7107dc65afed40970d0d493
suprit <supreeth@churchtalk.io>
2022-05-24 05:07:24 -0700

    Participation forum for the front-end

---
+++ frontend/src/components/Forums/Participation/PostNewQuestion.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Grid, TextField } from "@mui/material";
+import { createQuestion } from "../../../controllers/participationForum";
+
+export default function PostNewQuestion({
+  open,
+  eventId,
+  handleClose,
+  userId,
+}) {
+  const [questionText, setQuestionText] = useState(null);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let data = {};
+    data.text = questionText;
+    createQuestion(eventId, userId, data)
+      .then(() => {
+        handleClose();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <Dialog open={open} onClose={handleClose}>
+        <form onSubmit={onSubmit}>
+          <DialogTitle>Post new question</DialogTitle>
+          <DialogContent>
+            <Grid container>
+              <Grid item>
+                <TextField
+                  required
+                  label="Ask your question here"
+                  fullWidth
+                  multiline
+                  sx={{ width: "500px", marginTop: "15px" }}
+                  rows="3"
+                  autoComplete="family-name"
+                  variant="outlined"
+                  onChange={(e) => setQuestionText(e.target.value)}
+                />
+              </Grid>
+            </Grid>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              sx={{ textTransform: "none" }}
+              onClick={handleClose}
+            >
+              Close
+            </Button>
+            <Button
+              variant="contained"
+              sx={{ textTransform: "none" }}
+              type="submit"
+            >
+              Post
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    </div>
+  );
+}
---
+++ frontend/src/controllers/participationForum.js
@@ -0,0 +1,60 @@
+import { semiEndpoint } from "../utils/ApiEndpoint";
+import axios from "axios";
+
+export function getQuestionsByEvent(eventId, userId) {
+  return axios.get(
+    semiEndpoint + "/api/forums/participant/" + eventId + "/questions" + "?userId=" + userId
+  );
+}
+
+export function createQuestion(eventId, userId, dataJson) {
+  return axios.post(
+    semiEndpoint +
+      "/api/forums/participant/" +
+      eventId +
+      "/questions?text=" +
+      dataJson.text +
+      "&userId=" + 
+      userId
+  );
+}
+
+export function createAnswer(questionId, dataJson) {
+  return axios.post(
+    semiEndpoint +
+      "/api/forums/participant/questions/" +
+      questionId +
+      "/answers?text=" +
+      dataJson.text +
+      "&userId=" +
+      dataJson.userId
+  );
+}
+
+export function getQuestionAnswers(questionId, userId) {
+  return axios.get(
+    semiEndpoint + "/api/forums/participant/questions/" + questionId + "/answers" + "?userId=" + userId
+  );
+}
+
+export function closeForum(userId, eventId, text) {
+  return axios.post(
+    `${semiEndpoint}/api/forums/participant/${eventId}/cancel?text=${text}&userId=${userId}`
+  )
+};
+
+export function canPostToForum(event) {
+  if (!event) return false;
+  const {pForumOpen, endTime} = event;
+
+  if (pForumOpen === false)
+    return false;
+
+  let eventEndTime = new Date(endTime);
+  eventEndTime.setTime(eventEndTime.getTime() + (1000 * 60 * 60 * 24 * 3));
+
+  if (new Date().getTime() > eventEndTime) 
+    return false;
+
+  return true;
+};
