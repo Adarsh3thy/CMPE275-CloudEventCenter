45534eb95fd503f157f07cc66e2245af77cd7e3f
Anay Dilip Naik <89814957+anaydilipnaik@users.noreply.github.com>
2022-05-23 17:09:33 -0700

    reports integrated

--- frontend/src/components/Reports/index.js
+++ frontend/src/components/Reports/index.js
@@ -1,34 +1,113 @@
-import { Grid } from "@mui/material";
+import * as React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 import { useState, useEffect } from "react";
 import {
   getSystemReport,
   getParticipantReport,
   getOrganizerReport,
 } from "../../controllers/reports";
+import { AuthConsumer } from "../contexts/Auth/AuthContext";
 
-const Reports = () => {
+const Reports = ({ user }) => {
+  const [systemReport, setSystemReport] = useState(null);
+  const [participantReport, setParticipantReport] = useState(null);
+  const [organizerReport, setOrganizerReport] = useState(null);
+
   useEffect(() => {
-    getSystemReport()
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  }, []);
+    if (user) {
+      getSystemReport()
+        .then((res) => {
+          setSystemReport(res.data);
+          return getParticipantReport(user.id);
+        })
+        .then((res) => {
+          setParticipantReport(res.data);
+          return getOrganizerReport(user.id);
+        })
+        .then((res) => {
+          setOrganizerReport(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
 
   return (
-    <Grid container direction="column">
-      <Grid item container direction="row">
-        <Grid item style={{ width: 500, height: 500 }}>
-          <h3 style={{ fontWeight: "bold", textDecoration: "underline" }}>
-            System Report
-          </h3>
-        </Grid>
-        <Grid item style={{ width: 500, height: 500 }}>
-          <h3 style={{ fontWeight: "bold", textDecoration: "underline" }}>
-            Participant Report
-          </h3>
-        </Grid>
-      </Grid>
-    </Grid>
+    <>
+      <h1>System Report</h1>
+      {systemReport ? (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 450 }} aria-label="simple table">
+            <TableBody>
+              {Object.keys(systemReport).map((key) => (
+                <TableRow
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {key}
+                  </TableCell>
+                  <TableCell align="right">{systemReport[key]}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : null}
+      <br />
+      <br />
+      <h1>Participant Report</h1>
+      {participantReport ? (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 450 }} aria-label="simple table">
+            <TableBody>
+              {Object.keys(participantReport).map((key) => (
+                <TableRow
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {key}
+                  </TableCell>
+                  <TableCell align="right">{participantReport[key]}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : null}
+      <br />
+      <br />
+      <h1>Organizer Report</h1>
+      {organizerReport ? (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 450 }} aria-label="simple table">
+            <TableBody>
+              {Object.keys(organizerReport).map((key) => (
+                <TableRow
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {key}
+                  </TableCell>
+                  <TableCell align="right">{organizerReport[key]}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : null}
+    </>
   );
 };
 
-export default Reports;
+export default (props) => (
+  <AuthConsumer>
+    {({ user }) => <Reports user={user} {...props} />}
+  </AuthConsumer>
+);
